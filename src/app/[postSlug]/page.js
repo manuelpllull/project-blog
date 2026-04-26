e6723fb24bf64b31c9d3e41b616dc5ca6dcfab33
Manuel Peña Llull <manuel.pe97a.aema@gmail.com>
2026-04-26 20:11:19 +0200

    Exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { cache } from 'react';
 
 import BlogHero from '@/components/BlogHero';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import { BLOG_TITLE } from '@/constants';
 
 import styles from './postSlug.module.css';
+
+const cachedLoadBlogPost = cache(loadBlogPost);
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = params;
+
+  try {
+    const { frontmatter } = await cachedLoadBlogPost(postSlug);
+
+    return {
+      title: `${frontmatter.title} • ${BLOG_TITLE}`,
+      description: frontmatter.abstract,
+    };
+  } catch (error) {
+    return {
+      title: BLOG_TITLE,
+      description: 'A wonderful blog about JavaScript',
+    };
+  }
+}
 
 export default async function BlogPost({ params }) {
   const { postSlug } = params;
